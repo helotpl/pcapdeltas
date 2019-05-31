829c43f9fdbec169e727044f724bd93a4ef78939
helotpl <barczyk@gmail.com>
2019-05-31 10:32:29 +0200

    first commit

---
+++ pcapdeltas.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	var handle *pcap.Handle
+	var err error
+
+	flag.Parse()
+
+	files := flag.Args()
+
+	for i := range files {
+		file := files[i]
+		handle, err = pcap.OpenOffline(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer handle.Close()
+
+		deltas := make([]string, 0)
+		var last *time.Time
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			ts := packet.Metadata().CaptureInfo.Timestamp
+			if last == nil {
+				last = &ts
+			} else {
+				deltas = append(deltas, strconv.FormatInt(ts.Sub(*last).Nanoseconds()/1000, 10))
+				last = &ts
+			}
+		}
+		//fmt.Printf("%#v\n", deltas)
+		fmt.Println(strings.Join(deltas, ","))
+	}
+}
